e56aa8a64839f9b2c71b0ed9b01e5984f2136b8f
deanyao1986 <deanyao1986@hotmail.com>
2024-10-22 14:04:58 +0800

    解决

--- components/GisDemo.vue
+++ components/GisDemo.vue
@@ -1,47 +1,47 @@
-// 创建一个简单的案例 ,加在页面上
 <template>
     <div id="container">
-        <div id="viewDiv"></div>
+      <div id="viewDiv"></div>
     </div>
-    
-</template>
-
-<script setup>
-import { onMounted } from 'vue'
-import Map from '@arcgis/core/Map'
-import MapView from '@arcgis/core/views/MapView'
-// import '@arcgis/core/assets/esri/themes/light/main.css'
-
-onMounted(() => {
-    const map = new Map({
-        basemap: 'streets-navigation-vector'
-    })
-    const view = new MapView({
-        container: 'viewDiv',
-        map,
-        center: [120, 30],
-        zoom: 5
-    })
-    // view.ui.remove('attribution')
+  </template>
+  
+  <script setup>
+  import { onMounted } from 'vue';
+  
+  onMounted(async () => {
+    const [Map, MapView] = await Promise.all([
+      import('@arcgis/core/Map'),
+      import('@arcgis/core/views/MapView')
+    ]);
+  
+    const map = new Map.default({
+      basemap: 'streets-navigation-vector'
+    });
+  
+    const view = new MapView.default({
+      container: 'viewDiv',
+      map,
+      center: [120, 30],
+      zoom: 5
+    });
+  
+    // view.ui.remove('attribution');
     view.when(() => {
-        console.log('view ready')
-    })
-})
-    
-    
-</script>
-
-<style lang="scss" >
-
-#container,#viewDiv {
+      console.log('view ready');
+    });
+  });
+  </script>
+  
+  <style lang="scss">
+  #container, #viewDiv {
     width: 100%;
     height: 500px;
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-}
-
-#container {
+  }
+  
+  #container {
     position: relative;
-}
-</style>
+  }
+  </style>
+  
---
+++ docs/basement/git入门.md
@@ -0,0 +1,12 @@
+# Git 教程
+待完善
+
+## 提交失败
+
+1. 开启v2rayn的局域网代理
+2. 设置git 的代理地址
+```
+git config --global http.proxy http://127.0.0.1:10809
+git config --global https.proxy http://127.0.0.1:10809
+
+```
